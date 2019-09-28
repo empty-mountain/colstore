195828faad261ad131989904df297ed75916d0ec
empty-mountain <820459646@qq.com>
2019-09-28 14:39:26 +0800

    尝试连接并创建分支

---
+++ TestFile.py
@@ -0,0 +1,17 @@
+"""test something here。
+"""
+
+
+class test(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def knows(xs):
+        print('jijijijiji%s'%xs)
+
+
+test.knows(3)
+
+
+
