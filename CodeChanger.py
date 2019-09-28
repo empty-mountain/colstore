29bb660241e26ac3020283489d53d6e48e537e50
empty-mountain <820459646@qq.com>
2019-09-28 15:03:26 +0800

    20%

---
+++ CodeChanger.py
@@ -0,0 +1,39 @@
+"""本模块用于完成密码的加密解密功能
+
+1.随机密码构成
+    根据输入选择密码构成
+    加入密码对象，日期
+    随机插入字符
+
+2.暂时选用加密程度较低的base64加密
+"""
+import base64
+class base64_code(object):
+    """base64加密解密程序"""
+    def __init__(self,real_code):
+        self.code = real_code
+        
+    def encrypt(self):
+        #base64加密程序
+        # 编译为byte类型
+        code = self.code.encode('utf8')
+        #加密
+        encrypt_code = base64.b64encode(code)
+        return encrypt_code
+
+    def declassified(self,encrypt_code):
+        # base64解密程序
+        decode = base64.b64decode(encrypt_code)
+        return decode
+
+if __name__ == "__main__":
+    #实例化一个对象
+    code = input('input your code here')
+
+    coding = base64_code(code)
+    coded = coding.encrypt()
+    print(coded)
+    mid = coding.declassified(coded)
+    mids = str(mid,encoding='utf-8')
+    print(mids)
+    
---
+++ RandomCode.py
@@ -0,0 +1,72 @@
+"""本文档为主函数，用于调用及交互等功能。
+
+1、密码管理器访问权限
+2、根据用户输入调用相关功能
+
+"""
+
+import random
+
+
+class InputManage():
+    """用于生成与管理输入输出"""
+    
+    def __init__(self,middle_linked):
+        self.middle_linked = middle_linked
+
+    def DealInput(self):
+        #使用split分割输入
+        command_list = self.middle_linked.split('.',1)
+        #调用方法处理
+        pass_permit = self.is_user(command_list[0])
+        if pass_permit == 1 and command_list[1] !=0:
+            pass_word = self.add_symbol()
+            return pass_word
+
+        elif pass_permit == 1 :
+            pass
+        
+        else: 
+            print('password error')
+            return 0
+            
+
+    def is_user(self,password):
+        #验证密码，正确返回1
+        if password == 'aaa':
+            return 1
+        else: return 0
+
+    def add_symbol(self):
+        #随机加入符号
+        symbol_list = ['#','$','!','@','*','*']
+        pass_code = 'co'
+        while len(pass_code) < 12:
+            num_1 = random.randrange(1, 30, 1)
+            num_2 = random.randrange(1, 30, 1)
+            nums_all = num_1*num_2
+
+            symbol_choice = random.choice(symbol_list)
+            pass_code = pass_code + str(nums_all) + symbol_choice
+        
+        return pass_code
+
+
+# kdkddkkdkdk
+if __name__ == "__main__":
+    
+    print('1、请输入密码\n')
+    print('2、是否加入符号\n')
+    # print('如果需要')
+    
+    #实例化循环执行
+    while 1:
+        #获得输入
+        middle_link = input('请输入指令')
+        middle_object = InputManage(str(middle_link))
+        pass_code = middle_object.DealInput()
+        #判断，执行成功则输出
+        if pass_code !=0:
+            print(pass_code)
+            break
+
